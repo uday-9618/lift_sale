87fccf755ca9bff76f97ff719d2771177a415197
Akshaya Javvaji <javvajiakshaya07@gmail.com>
2025-10-30 22:44:44 +0530

    quotes1

--- lib/main.dart
+++ lib/main.dart
@@ -35,8 +35,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       //home: leaddetails(),
-      //home: const MyHomePage(title: 'Flutter Demo Home Page'),
-      home: quotes(),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      //home: quotes(),
     );
   }
 }
